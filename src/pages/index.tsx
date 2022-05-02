788b8ef7627d10a35838fb1ff2053b6ce2f2fa1d
Tiaguin061 <tiaguin180@gmail.com>
2022-05-01 21:35:00 -0300

    feat: Index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
