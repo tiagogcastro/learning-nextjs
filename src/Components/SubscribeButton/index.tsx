0c5552afa87aaf4d2b79803c2f230d8d5c7b3caf
Tiaguin061 <tiaguin180@gmail.com>
2022-05-18 11:45:03 -0300

    feat: Starting handle subscribe

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -7,10 +8,19 @@ interface SubscribeButtonProps {
 export function SubscribeButton({
   priceId
 }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+
+  function handleSubscribe() {
+    if(!session) {
+      return signIn('github');
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
