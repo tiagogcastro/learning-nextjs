2d76683d466ab23dba75ea23610bf55947b19f91
Tiaguin061 <tiaguin180@gmail.com>
2022-06-12 13:44:17 -0300

    feat: Consum api to stripe subscribe

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -10,9 +12,23 @@ export function SubscribeButton({
 }: SubscribeButtonProps) {
   const { data: session } = useSession()
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       return signIn('github');
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({
+        sessionId
+      });
+    } catch (error: any) {
+      alert(error.message);
     }
   }
 
