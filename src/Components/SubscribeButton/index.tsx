725fb498707d505f1e8fda89c034467dac9fb5c3
Tiaguin061 <tiaguin180@gmail.com>
2022-08-09 13:37:00 -0300

    feat: Redirect user to posts page if has active subscription

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -11,10 +12,16 @@ export function SubscribeButton({
   priceId
 }: SubscribeButtonProps) {
   const { data: session } = useSession()
+  const router = useRouter();
 
   async function handleSubscribe() {
     if(!session) {
       return signIn('github');
+    }
+
+    if(session.activeSubscription) {
+      router.push('/posts');
+      return;
     }
 
     try {
