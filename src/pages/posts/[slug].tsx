c34526c7d366ad6fc1a04005deca55a709a7e76e
Tiaguin061 <tiaguin180@gmail.com>
2022-08-09 13:37:49 -0300

    feat: Redirect user to home page if not has active subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -48,9 +48,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
   const { slug } = params;
 
-  // if(!session) {
-  //   -
-  // }
+  if(!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    };
+  }
 
   const prismic = getPrismicClient();
 
