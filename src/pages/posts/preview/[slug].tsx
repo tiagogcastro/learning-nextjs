203e894c6ab3d05f23a6aced38e219b68be88e45
Tiaguin061 <tiaguin180@gmail.com>
2022-08-10 11:29:07 -0300

    feat: Module Finalized

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { getSession, useSession } from 'next-auth/react'
 import Head from 'next/head';
 import Link from 'next/link';
@@ -59,7 +59,7 @@ export default function PostPreview({
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
     fallback: 'blocking',
@@ -87,6 +87,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post
-    }
+    },
+    revalidate: 60 * 30, // 30 minutos
   }
 }
