13de5a7f799833aab9ada6356ced0f19654ffea3
Tiaguin061 <tiaguin180@gmail.com>
2022-08-11 10:29:14 -0300

    feat: Signin page started

---
+++ src/pages/index.tsx
@@ -0,0 +1,70 @@
+import type { NextPage } from 'next';
+
+import { Button, Flex, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+
+const Home: NextPage = () => {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack
+          spacing="4"
+        >
+          <FormControl>
+            <FormLabel htmlFor='email'>E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg" 
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor='password'>Password</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button 
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Home;
