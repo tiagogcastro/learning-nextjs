bf61a2b0d75eba2e3276a42c443565ea1d714ea6
Tiaguin061 <tiaguin180@gmail.com>
2022-08-12 13:04:04 -0300

    feat: Using sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,24 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { SideBar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex
+      direction="column"
+      h="100vh"
+    >
       <Header />
-    </>
+      
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <SideBar />
+      </Flex>
+    </Flex>
   )
 }
