9c4536983672f12b2b911d3bb234c04024c87268
Tiaguin061 <tiaguin180@gmail.com>
2022-08-23 10:21:35 -0300

    feat: Changed box padding if not wide version

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignContent="flex-start">
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
@@ -96,7 +96,7 @@ export default function Dashboard() {
             />
           </Box>
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
