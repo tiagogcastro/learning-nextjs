50c1f7b8a6bca05d9e61f55dd756929a0da31d5e
Tiaguin061 <tiaguin180@gmail.com>
2022-08-23 11:08:28 -0300

    improve: Create user page responsive

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -21,7 +21,7 @@ export default function CreateUser() {
           flex="1"
           borderRadius={8}
           bg="gray.800"
-          p="8"
+          p={["6", "8"]}
         >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
@@ -30,21 +30,40 @@ export default function CreateUser() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid
+              minChildWidth="240px"
+              spacing={["6", "8"]}
+              w="100%"
+            >
               <Input name="name" label="Nome completo" />
               <Input name="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid
+              minChildWidth="240px"
+              spacing={["6",
+              "8"]}
+              w="100%"
+            >
               <Input name="password" label="Senha" type="password" />
               <Input name="password_confirmation" label="Confirmação da senha" type="password" />
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
-            <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
-              <Button colorScheme="pink">Salvar</Button>
+          <Flex mt="8" justify={["center", "flex-end"]}>
+            <HStack spacing="4" width={["100%", "auto"]}>
+              <Button
+                width={["100%", "auto"]}
+                colorScheme="whiteAlpha"
+              >
+                Cancelar
+              </Button>
+              <Button
+                width={["100%", "auto"]}
+                colorScheme="pink"
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
