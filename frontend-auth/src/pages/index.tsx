1b07713408877c2789edd251402525997bc08bd5
Tiaguin061 <tiaguin180@gmail.com>
2022-09-11 13:17:39 -0300

    feat: Created function to validate guest user in SSR

--- frontend-auth/src/pages/index.tsx
+++ frontend-auth/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { parseCookies } from 'nookies';
 import React, { useState } from 'react';
+import { withSSRGuest } from 'utils/withSSRGuest';
 
 export default function Home () {
   const [email, setEmail] = useState('');
@@ -42,21 +43,10 @@ export default function Home () {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if(cookies['nextAuth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      },
-    }
-  }
-
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {
-
+      
     }
   }
-}
+})
